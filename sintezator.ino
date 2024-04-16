fbd1d30b2b7658f8bb4e9859107de14217902170
Toster <154697128+TOOSTEER@users.noreply.github.com>
2024-04-16 10:34:26 +0300

    sintezator.ino

--- sintezator.ino
+++ sintezator.ino
@@ -1,21 +1,18 @@
-#include <Adafruit_NeoPixel.h>
-unsigned long timing_const = 0;
-int pix = 360;
+#include <Adafruit_NeoPixel.h> //Иницилизация библиотки Adafruit_NeoPixel
+unsigned long timing_const = 0;//Иницилизация статичной переменной времени
+int pix = 360; //Инициилизируем общее количество светодиодов
 Adafruit_NeoPixel strip(pix, 6, NEO_RGB + NEO_KHZ400);
 void setup() {
   Serial.begin(9600);
   strip.begin();
   strip.show();
-  strip.setBrightness(30);
-
+  strip.setBrightness(50); //Установление определённой яркости
 }
-
 void loop() {
-
 }
-void ful_note(int pin_num) {
-  unsigned long timing_run = millis();
-  for (pin_num; pin_num >= pin_num - 5; pin_num --) {
+void ful_note(int pin_num) { //Создание функции для вызова целой ноты
+  unsigned long timing_run = millis(); //Инициилизация "бегущей" перемены времени
+  for (pin_num; pin_num >= pin_num - 5; pin_num --) { //Создание цикла для "падения" светодиода до "нижней" позиции
     strip.setPixelColor(pin_num, 250, 0, 0);
     if (timing_run - timing_const == 250) {
       timing_const = timing_run;
@@ -23,13 +20,13 @@ void ful_note(int pin_num) {
       strip.setPixelColor(pin_num - 1, 250, 0, 0);
     }
   }
-  if (timing_run - timing_const == 4000) {
+  if (timing_run - timing_const == 4000) { //Установление времени "исчезновения" светодиода
     strip.clear();
   }
   strip.show();
 }
-void note_2(int pin_num) {
-  unsigned long timing_run = millis();
+void note_2(int pin_num) { //Создание функции для вызова половинной ноты
+  unsigned long timing_run = millis(); //Инициилизация "бегущей" перменной ноты
   for (pin_num; pin_num >= pin_num - 5; pin_num --) {
     for (pin_num; pin_num >= pin_num - 1; pin_num --) {
       strip.setPixelColor(pin_num, 250, 0, 0);
@@ -38,20 +35,20 @@ void note_2(int pin_num) {
         strip.setPixelColor(pin_num - 1, 250, 0, 0);
       }
     }
-    if (timing_run - timing_const == 250) {
+    if (timing_run - timing_const == 250) { 
       timing_run = timing_const;
       strip.setPixelColor(pin_num, 0, 0, 0);
       strip.setPixelColor(pin_num - 2, 250, 0, 0);
     }
   }
-  if (timing_run - timing_const == 2000) {
+  if (timing_run - timing_const == 2000) { //Установление времени "исчезновения" ноты
     timing_run = timing_const;
     strip.clear();
   }
   strip.show();
 }
-void note_4(int pin_num) {
-  unsigned long timing_run = millis();
+void note_4(int pin_num) { //Создание функции вызова четвёртой ноты
+  unsigned long timing_run = millis(); //Инициилизация "бегущей" переменной времени
   for (pin_num; pin_num >= pin_num - 5; pin_num --) {
     for (pin_num; pin_num >= pin_num - 2; pin_num --) {
       strip.setPixelColor(pin_num, 250, 0, 0);
@@ -65,14 +62,14 @@ void note_4(int pin_num) {
       strip.setPixelColor(pin_num - 3, 250, 0, 0);
     }
   }
-  if (timing_run - timing_const == 1000) {
+  if (timing_run - timing_const == 1000) { //Установление времени "исчезновения" ноты
     timing_run = timing_const;
     strip.clear();
   }
   strip.show();
 }
-void note_8(int pin_num) {
-  unsigned long timing_run = millis();
+void note_8(int pin_num) { //Создание функции для вызова восьмой ноты
+  unsigned long timing_run = millis(); //Инициилизация "бегущей" переменной нот
   for (pin_num; pin_num >= pin_num - 5; pin_num --) {
     for (pin_num; pin_num >= pin_num - 3; pin_num --) {
       strip.setPixelColor(pin_num, 250, 0, 0);
@@ -87,13 +84,13 @@ void note_8(int pin_num) {
       strip.setPixelColor(pin_num - 4, 250, 0, 0);
     }
   }
-  if (timing_run - timing_const == 500) {
+  if (timing_run - timing_const == 500) { //Установление времени "исчезновения" ноты
     strip.clear();
   }
   strip.show();
 }
-void note_16(int pin_num) {
-  unsigned long timing_run = millis();
+void note_16(int pin_num) { //Создание функции для вызова шестнадцатой ноты
+  unsigned long timing_run = millis(); //Инициилизация "бегущей" переменной времени
   for (pin_num; pin_num >= pin_num - 5; pin_num --) {
     for (pin_num; pin_num >= pin_num - 4; pin_num --) {
       strip.setPixelColor(pin_num, 250, 0, 0);
@@ -108,13 +105,13 @@ void note_16(int pin_num) {
       strip.setPixelColor(pin_num - 5, 250, 0, 0);
     }
   }
-  if (timing_run - timing_const == 250) {
+  if (timing_run - timing_const == 250) {//Установление времени "исчезновения" ноты
     strip.clear();
   }
   strip.show();
 }
-void note_32(int pin_num){
-  unsigned long timing_run = millis();
+void note_32(int pin_num){ //Создание функции для вызова тридцать второй ноты
+  unsigned long timing_run = millis(); //Иницилизация "бегущей" переменной времени
   for (pin_num; pin_num >= pin_num - 5;pin_num --){
     strip.setPixelColor(pin_num,250,0,0);
     if (timing_run - timing_const == 250){
@@ -122,7 +119,7 @@ void note_32(int pin_num){
       strip.setPixelColor(pin_num - 1, 250,0,0);
     }
   }
-  if (timing_run - timing_const == 125){
+  if (timing_run - timing_const == 125){ //Установление времени "исчезновения" ноты
     strip.clear();
   }
   strip.show();
